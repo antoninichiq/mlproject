8de35a0428427dd130cfcf2821fdbdfb26a8eee3
antoninichiq <antonini.chiquetto@yahoo.com.br>
2024-06-24 10:43:24 -0300

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -51,7 +52,10 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
     
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_array,test_array,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_array,test_array))
+    
 # Needed to include dir 'src' in PYTHONPATH:  
 # set PYTHONPATH=%PYTHONPATH%;C:\Users\anton\OneDrive\Documentos\mlproject
